f07545f6f19401708c225f7185c56804d7a0942f
drupality <marekkisiel.dev@gmail.com>
2015-01-24 17:03:51 +0100

    Enable layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,14 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+
+  protected function _initDoctype()
+  {
+    $this->bootstrap('view');
+    $view = $this->getResource('view');
+    $view->doctype('HTML5');
+  }
+
   public function _initRoutes()
   {
     $frontController = Zend_Controller_Front::getInstance();
